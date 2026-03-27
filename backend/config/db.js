0637baffbb7177406e2ec3f69cfd95a82b99bf4b
Markus <sanmarkusmorcoso@gmail.com>
2026-03-27 08:14:38 +0800

    switch to connection pool for stability

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,13 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-});
-
-db.connect((err) => {
-  if (err) console.error("DB Connection Failed:", err);
-  else console.log("MySQL Connected");
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
 module.exports = db;
